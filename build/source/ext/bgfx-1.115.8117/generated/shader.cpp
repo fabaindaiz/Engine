d0bd456f0c2ea728f7ff95cce3c32c5a27e1583f
fabaindaiz <fabiandiaz@ug.uchile.cl>
2022-04-28 13:11:56 -0400

    Added submodules

---
+++ build/source/ext/bgfx-1.115.8117/generated/shader.cpp
@@ -0,0 +1,4 @@
+#include "C:/Users/fabia/Desktop/CC5512Motor/source/ext/bgfx-1.115.8117/bgfx/src/shader.cpp"
+#include "C:/Users/fabia/Desktop/CC5512Motor/source/ext/bgfx-1.115.8117/bgfx/src/shader_dx9bc.cpp"
+#include "C:/Users/fabia/Desktop/CC5512Motor/source/ext/bgfx-1.115.8117/bgfx/src/shader_dxbc.cpp"
+#include "C:/Users/fabia/Desktop/CC5512Motor/source/ext/bgfx-1.115.8117/bgfx/src/shader_spirv.cpp"
---
+++ build/source/ext/bgfx-1.115.8117/generated/vertexlayout.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/fabia/Desktop/CC5512Motor/source/ext/bgfx-1.115.8117/bgfx/src/vertexlayout.cpp"
